525ff56718785a7b45b284b72b2015166ecda694
spandan12 <spandan.pyakurel.1996@gmail.com>
2019-10-02 07:53:12 +0545

    right part completed

--- js/cv.js
+++ js/cv.js
@@ -29,7 +29,7 @@ profile.appendChild(profile_text);
 
 //add contact section to the left div
 contact=document.createElement('div');
-contact.classList.add("profile");
+// contact.classList.add("profile");
 parent[0].appendChild(contact);
 
 contact_head=document.createElement('span');
@@ -49,7 +49,7 @@ for(i=0;i<3;i++){
 
 //add Academic Qualifications
 Qualification=document.createElement('div');
-Qualification.classList.add("profile");
+// Qualification.classList.add("profile");
 parent[0].appendChild(Qualification);
 
 q_head=document.createElement('span');
@@ -79,7 +79,7 @@ for(i=0;i<3;i++){
 
 //add honours and awards
 honors=document.createElement('div');
-honors.classList.add("profile");
+// honors.classList.add("profile");
 parent[0].appendChild(honors);
 
 h_head=document.createElement('span');
@@ -107,7 +107,7 @@ for(i=0;i<3;i++){
 
 //add Technical Skills
 technical=document.createElement('div');
-technical.classList.add("profile");
+// technical.classList.add("profile");
 parent[0].appendChild(technical);
 
 technical_head=document.createElement('span');
@@ -123,7 +123,7 @@ technical.appendChild(technical_text);
 
 //add Non Technical Skills
 ntechnical=document.createElement('div');
-ntechnical.classList.add("profile");
+// ntechnical.classList.add("profile");
 parent[0].appendChild(ntechnical);
 
 ntechnical_head=document.createElement('span');
@@ -136,3 +136,69 @@ ntechnical_text.classList.add("normal_text");
 ntechnical_text.classList.add("no-padding");
 ntechnical_text.innerHTML = ' Communication | Self Motivation | Adaptability | Leadership | Enthusiasm | Presentation ';
 ntechnical.appendChild(ntechnical_text);
+
+
+//add Projects in the right wrapper
+
+var parent2=document.getElementsByClassName('right-wrapper');
+projects=document.createElement('div');
+parent2[0].appendChild(projects); 
+
+pr_head=document.createElement('span');
+pr_head.classList.add("div_head");
+pr_head.innerHTML = 'Honours and Awards';
+projects.appendChild(pr_head);
+
+
+var list_of_pr=['Near-realtime Assessment of Fine Scale SpatioTemporal Weather Anamolies and Pollution Proxies in Kathmandu Valley,2019',
+                'Facial Landmarks Detection,2018',
+                'Smart Discussion Forum, 2018',
+                'Msc Teachers and Experts Database,2018',
+                'Number Plate Recognition System,2017',
+                'MeriSaathi App,2017',
+                'Bus Ticket Booking App,2016'
+            ];
+var pr_details=['My role in the project was to parse the data in the appropriate format, data cleaning, Pre-processing, integration of data from various sources,database handling and make APIs for FrontEnd rendering.',
+                'My role in the project was to extract frame from video and pre-process the frame for dataset preparation and to combine the output frames into video and dynamic overlaying of images(hat, glasses) into the original image.',
+                'My role in the project was analysis of the scope, usecase of project and prepare UML diagrams. Also, to handle the database of the project completely and implement search tags and some of the FrontEnd entities.',
+                 'My role in the project was to handle the database of the project',
+                 'My role in the project was to prepare model for OCR,extract number plate from the image, pre-process, segment the image and implement OCR. This project was demonstrated in Locus.',
+                 'My role in the project was to handle the database of the project as well as frontEnd part of the system. This project made us the winner of Spirathon.',
+                 'My role in the project was to handle the database of the project as well as frontEnd part of the system. This project was demonstrated in Locus.'   
+            ];
+var counterq=0;
+for(i=0;i<7;i++){
+    element2=document.createElement('span');
+    element2.classList.add("normal_text");
+    element2.innerHTML = list_of_pr[i];
+    projects.appendChild(element2);
+    
+    element3=document.createElement('span');
+    element3.classList.add("in_text");
+    element3.innerHTML = pr_details[i];
+    element2.appendChild(element3);
+
+}
+
+//add honours and awards
+conference=document.createElement('div');
+conference.classList.add("conference");
+parent2[0].appendChild(conference);
+
+c_head=document.createElement('span');
+c_head.classList.add("div_head");
+c_head.innerHTML = 'Conference Presentations';
+conference.appendChild(c_head);
+
+
+var list_of_c=['\'Sentence Ranking And Answer Pinpointing In Online Discussion Forums Utilising User-generated Metrics And Highlights\'. NASCOIT, 2018',
+                '\'Near-real Time Profiling Of Fine Scale Environmental Proxies Using Mobile Sensors Along Kathmandu Road Lines\',ICQRIT, 2019'];
+
+var counterq=0;
+for(i=0;i<2;i++){
+    element2=document.createElement('span');
+    element2.classList.add("normal_text");
+    element2.innerHTML = list_of_c[i];
+    conference.appendChild(element2);
+
+}
